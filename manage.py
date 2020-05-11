0e203afa5f44c93d516321c4d37f9e22c9553301
Bolaji Olajide <cooproton@gmail.com>
2020-05-11 18:08:05 +0100

    initial commit

---
+++ manage.py
@@ -0,0 +1,11 @@
+from flask_script import Server, Manager
+
+from app import create_app
+
+
+app = create_app()
+manager = Manager(app)
+manager.add_command("runserver", server)
+
+if __name__ == "__main__"):
+    manager.run()
